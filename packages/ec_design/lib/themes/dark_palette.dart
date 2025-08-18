5c5aeda9de89470fe42f871fbb103625c8aec65f
Kiet Pham <kiet.pham@asnet.com.vn>
2025-08-18 17:56:55 +0700

    create variable and update naming

--- packages/ec_design/lib/themes/dark_palette.dart
+++ packages/ec_design/lib/themes/dark_palette.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class EcDarkPalette {
+  static const int _redPrimaryColor = 0xFFe2594e;
+  static const int _orangePrimaryColor = 0xFFf59733;
+  static const int _errorPrimaryColor = 0xFFf34c3e;
+  static const int _whitePrimaryColor = 0xFFFFFFFF;
+  static const int _greenPrimaryColor = 0xFF55ba75;
+  static const int _grayPrimaryColor = 0xFFafafaf;
+  static const int _blackPrimaryColor = 0xFF4e4e4e;
+
   static const MaterialColor ecRed = MaterialColor(
-    0xFFe2594e,
+    _redPrimaryColor,
     <int, Color>{
       50: Color(0xFF5c140e),
       100: Color(0xFF781a13),
       200: Color(0xFF9b2218),
       300: Color(0xFFc72c1f),
       400: Color(0xFFdb3022),
-      500: Color(0xFFe2594e),
+      500: Color(_redPrimaryColor),
       600: Color(0xFFe7746b),
       700: Color(0xFFeea099),
       800: Color(0xFFf4bfba),
@@ -18,14 +26,14 @@ class EcDarkPalette {
   );
 
   static const MaterialColor ecOrange = MaterialColor(
-    0xFFf59733,
+    _orangePrimaryColor,
     {
       50: Color(0xFF663500),
       100: Color(0xFF854500),
       200: Color(0xFFac5900),
       300: Color(0xFFdc7200),
       400: Color(0xFFf27d00),
-      500: Color(0xFFf59733),
+      500: Color(_orangePrimaryColor),
       600: Color(0xFFf6a854),
       700: Color(0xFFf9c38a),
       800: Color(0xFFfbd7b0),
@@ -34,14 +42,14 @@ class EcDarkPalette {
   );
 
   static const MaterialColor ecError = MaterialColor(
-    0xFFf34c3e,
+    _errorPrimaryColor,
     {
       50: Color(0xFF650d06),
       100: Color(0xFF841108),
       200: Color(0xFFaa160a),
       300: Color(0xFFda1c0d),
       400: Color(0xFFf01f0e),
-      500: Color(0xFFf34c3e),
+      500: Color(_errorPrimaryColor),
       600: Color(0xFFf5695e),
       700: Color(0xFFf89890),
       800: Color(0xFFfabab4),
@@ -50,30 +58,30 @@ class EcDarkPalette {
   );
 
   static const MaterialColor ecWhite = MaterialColor(
-    0xFFFFFFFF,
+    _whitePrimaryColor,
     <int, Color>{
       50: Color(0xFF6b6b6b),
       100: Color(0xFF8c8c8c),
       200: Color(0xFFb5b5b5),
       300: Color(0xFFe8e8e8),
-      400: Color(0xFFFFFFFF),
-      500: Color(0xFFFFFFFF),
-      600: Color(0xFFFFFFFF),
-      700: Color(0xFFFFFFFF),
-      800: Color(0xFFFFFFFF),
-      900: Color(0xFFFFFFFF),
+      400: Color(_whitePrimaryColor),
+      500: Color(_whitePrimaryColor),
+      600: Color(_whitePrimaryColor),
+      700: Color(_whitePrimaryColor),
+      800: Color(_whitePrimaryColor),
+      900: Color(_whitePrimaryColor),
     },
   );
 
   static const MaterialColor ecGreen = MaterialColor(
-    0xFF55ba75,
+    _greenPrimaryColor,
     <int, Color>{
       50: Color(0xFF124722),
       100: Color(0xFF175d2d),
       200: Color(0xFF1e783a),
       300: Color(0xFF269a4b),
       400: Color(0xFF2aa952),
-      500: Color(0xFF55ba75),
+      500: Color(_greenPrimaryColor),
       600: Color(0xFF70c58b),
       700: Color(0xFF9dd7af),
       800: Color(0xFFbde4c9),
@@ -82,14 +90,14 @@ class EcDarkPalette {
   );
 
   static const MaterialColor ecGrey = MaterialColor(
-    0xFFafafaf,
+    _grayPrimaryColor,
     <int, Color>{
       50: Color(0xFF414141),
       100: Color(0xFF555555),
       200: Color(0xFF6e6e6e),
       300: Color(0xFF8d8d8d),
       400: Color(0xFF9B9B9B),
-      500: Color(0xFFafafaf),
+      500: Color(_grayPrimaryColor),
       600: Color(0xFFbcbcbc),
       700: Color(0xFFd1d1d1),
       800: Color(0xFFE0E0E0),
@@ -98,14 +106,14 @@ class EcDarkPalette {
   );
 
   static const MaterialColor ecBlack = MaterialColor(
-    0xFF4e4e4e, // default
+    _blackPrimaryColor, // default
     <int, Color>{
       50: Color(0xFF0e0e0e),
       100: Color(0xFF131313),
       200: Color(0xFF181818),
       300: Color(0xFF1f1f1f),
       400: Color(0xFF222222),
-      500: Color(0xFF4e4e4e),
+      500: Color(_blackPrimaryColor),
       600: Color(0xFF6b6b6b),
       700: Color(0xFF999999),
       800: Color(0xFFbababa),
--- packages/ec_design/lib/themes/light_palette.dart
+++ packages/ec_design/lib/themes/light_palette.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class EcLightPalette {
+  static const int _redPrimaryColor = 0xFFDB3022;
+  static const int _orangePrimaryColor = 0xFFF27D00;
+  static const int _errorPrimaryColor = 0xFFf01f0e;
+  static const int _whitePrimaryColor = 0xFFFFFFFF;
+  static const int _greenPrimaryColor = 0xFF2AA952;
+  static const int _grayPrimaryColor = 0xFF9B9B9B;
+  static const int _blackPrimaryColor = 0xFF222222;
+
   static const MaterialColor ecRed = MaterialColor(
-    0xFFDB3022,
+    _redPrimaryColor,
     <int, Color>{
       50: Color(0xFFfbeae9),
       100: Color(0xFFf4bfba),
       200: Color(0xFFeea099),
       300: Color(0xFFe7746b),
       400: Color(0xFFe2594e),
-      500: Color(0xFFdb3022),
+      500: Color(_redPrimaryColor),
       600: Color(0xFFc72c1f),
       700: Color(0xFF9b2218),
       800: Color(0xFF781a13),
@@ -18,14 +26,14 @@ class EcLightPalette {
   );
 
   static const MaterialColor ecOrange = MaterialColor(
-    0xFFF27D00,
+    _orangePrimaryColor,
     {
       50: Color(0xFFfef2e6),
       100: Color(0xFFfbd7b0),
       200: Color(0xFFf9c38a),
       300: Color(0xFFf6a854),
       400: Color(0xFFf59733),
-      500: Color(0xFFf27d00),
+      500: Color(_orangePrimaryColor),
       600: Color(0xFFdc7200),
       700: Color(0xFFac5900),
       800: Color(0xFF854500),
@@ -34,14 +42,14 @@ class EcLightPalette {
   );
 
   static const MaterialColor ecError = MaterialColor(
-    0xFFF27D00,
+    _errorPrimaryColor,
     {
       50: Color(0xFFfee9e7),
       100: Color(0xFFfabab4),
       200: Color(0xFFf89890),
       300: Color(0xFFf5695e),
       400: Color(0xFFf34c3e),
-      500: Color(0xFFf01f0e),
+      500: Color(_errorPrimaryColor),
       600: Color(0xFFda1c0d),
       700: Color(0xFFaa160a),
       800: Color(0xFF841108),
@@ -50,14 +58,14 @@ class EcLightPalette {
   );
 
   static const MaterialColor ecWhite = MaterialColor(
-    0xFFFFFFFF,
+    _whitePrimaryColor,
     <int, Color>{
-      50: Color(0xFFFFFFFF),
-      100: Color(0xFFFFFFFF),
-      200: Color(0xFFFFFFFF),
-      300: Color(0xFFFFFFFF),
-      400: Color(0xFFFFFFFF),
-      500: Color(0xFFFFFFFF),
+      50: Color(_whitePrimaryColor),
+      100: Color(_whitePrimaryColor),
+      200: Color(_whitePrimaryColor),
+      300: Color(_whitePrimaryColor),
+      400: Color(_whitePrimaryColor),
+      500: Color(_whitePrimaryColor),
       600: Color(0xFFe8e8e8),
       700: Color(0xFFb5b5b5),
       800: Color(0xFF8c8c8c),
@@ -66,14 +74,14 @@ class EcLightPalette {
   );
 
   static const MaterialColor ecGreen = MaterialColor(
-    0xFF2AA952,
+    _greenPrimaryColor,
     <int, Color>{
       50: Color(0xFFeaf6ee),
       100: Color(0xFFbde4c9),
       200: Color(0xFF9dd7af),
       300: Color(0xFF70c58b),
       400: Color(0xFF55ba75),
-      500: Color(0xFF2aa952),
+      500: Color(_greenPrimaryColor),
       600: Color(0xFF269a4b),
       700: Color(0xFF1e783a),
       800: Color(0xFF175d2d),
@@ -82,14 +90,14 @@ class EcLightPalette {
   );
 
   static const MaterialColor ecGrey = MaterialColor(
-    0xFF9B9B9B,
+    _grayPrimaryColor,
     <int, Color>{
       50: Color(0xFFF5F5F5),
       100: Color(0xFFE0E0E0),
       200: Color(0xFFd1d1d1),
       300: Color(0xFFbcbcbc),
       400: Color(0xFFafafaf),
-      500: Color(0xFF9B9B9B),
+      500: Color(_grayPrimaryColor),
       600: Color(0xFF8d8d8d),
       700: Color(0xFF6e6e6e),
       800: Color(0xFF555555),
@@ -98,14 +106,14 @@ class EcLightPalette {
   );
 
   static const MaterialColor ecBlack = MaterialColor(
-    0xFF222222,
+    _blackPrimaryColor,
     <int, Color>{
       50: Color(0xFFe9e9e9),
       100: Color(0xFFbababa),
       200: Color(0xFF999999),
       300: Color(0xFF6b6b6b),
       400: Color(0xFF4e4e4e),
-      500: Color(0xFF222222),
+      500: Color(_blackPrimaryColor),
       600: Color(0xFF1f1f1f),
       700: Color(0xFF181818),
       800: Color(0xFF131313),
