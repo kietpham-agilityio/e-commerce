4746cf2602c22cb2669a5b7a9c368a3349b66b41
Kiet Pham <kiet.pham@asnet.com.vn>
2025-08-20 17:34:57 +0700

    define the app sizing for both admin and user

---
+++ packages/ec_design/lib/themes/app_sizing.dart
@@ -0,0 +1,74 @@
+import 'app_colors.dart';
+import 'sizing/admin_sizing.dart';
+import 'sizing/user_sizing.dart';
+
+class AppSizing {
+  const AppSizing(this.type);
+
+  final ECThemeType type;
+
+  double get switcher => switch (type) {
+        ECThemeType.admin => AdminSizing.switcher,
+        ECThemeType.user => UserSizing.switcher,
+      };
+
+  double get checkbox => switch (type) {
+        ECThemeType.admin => AdminSizing.checkbox,
+        ECThemeType.user => UserSizing.checkbox,
+      };
+
+  double get iconSlider => switch (type) {
+        ECThemeType.admin => AdminSizing.iconSlider,
+        ECThemeType.user => UserSizing.iconSlider,
+      };
+
+  double get appBarHeight => switch (type) {
+        ECThemeType.admin => AdminSizing.appBar,
+        ECThemeType.user => UserSizing.appBar,
+      };
+
+  double get tag => switch (type) {
+        ECThemeType.admin => AdminSizing.tag,
+        ECThemeType.user => UserSizing.tag,
+      };
+
+  double get dropdown => switch (type) {
+        ECThemeType.admin => AdminSizing.dropdown,
+        ECThemeType.user => UserSizing.dropdown,
+      };
+
+  double get iconButtonSmall => switch (type) {
+        ECThemeType.admin => AdminSizing.iconButtonSmall,
+        ECThemeType.user => UserSizing.iconButtonSmall,
+      };
+
+  double get iconButtonBig => switch (type) {
+        ECThemeType.admin => AdminSizing.iconButtonBig,
+        ECThemeType.user => UserSizing.iconButtonBig,
+      };
+
+  double get buttonSmall => switch (type) {
+        ECThemeType.admin => AdminSizing.buttonSmall,
+        ECThemeType.user => UserSizing.buttonSmall,
+      };
+
+  double get button => switch (type) {
+        ECThemeType.admin => AdminSizing.button,
+        ECThemeType.user => UserSizing.button,
+      };
+
+  double get searchBar => switch (type) {
+        ECThemeType.admin => AdminSizing.searchBar,
+        ECThemeType.user => UserSizing.searchBar,
+      };
+
+  double get textFieldSmall => switch (type) {
+        ECThemeType.admin => AdminSizing.textFieldSmall,
+        ECThemeType.user => UserSizing.textFieldSmall,
+      };
+
+  double get textFieldOrdinary => switch (type) {
+        ECThemeType.admin => AdminSizing.textFieldOrdinary,
+        ECThemeType.user => UserSizing.textFieldOrdinary,
+      };
+}
