a2a7ddad39c732c9c5ea7a883e2000886a5051cf
Kiet Pham <kiet.pham@asnet.com.vn>
2025-09-18 17:33:57 +0700

    inprogress setup notification

---
+++ packages/ec_core/lib/services/ec_notifications/config/notification_config.dart
@@ -0,0 +1,7 @@
+class NotificationConfig {
+  static String channelKey = 'E_Commerce_App_Channel_Key';
+  static String channelName = 'E_Commerce_App_Channel_Name';
+  static String channelDescription = 'E_Commerce_App_Channel_Description';
+  static String channelGroupKey = 'E_Commerce_App_Channel_Group_Key';
+  static String channelGroupName = 'E_Commerce_App_Channel_Group_Name';
+}
