ff4920be6b202dfa895534c6c18f9e2c0c3c71f1
Kiet Pham <kiet.pham@asnet.com.vn>
2025-09-19 17:42:23 +0700

    inprogress implement cache for notification

---
+++ packages/ec_core/lib/services/ec_notifications/handler/notification_handler.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:ec_core/services/ec_notifications/models/notification_models.dart';
+
+class NotificationHandler {
+  static void handleTapNavigate(
+    Map<String, dynamic> data,
+    NotificationEntity entity,
+  ) {
+    final response = NotificationResponseData.fromJson(data);
+    final type = response.type ?? '';
+
+    if (type.isNotEmpty) {
+      entity.onTap?.call(
+        NotificationsResponseEntity(
+          type: type,
+          orderId: response.orderId ?? '',
+        ),
+      );
+    }
+  }
+
+  static void navigate({
+    required NotificationsResponseEntity notification,
+    void Function(NotificationsResponseEntity)? onTap,
+  }) {
+    try {
+      switch (notification.type) {
+        case NotificationType.orderDetails:
+          onTap?.call(notification);
+          break;
+        default:
+          log('Unknown notification type: ${notification.type}');
+      }
+    } catch (e) {
+      log('Navigation error: $e');
+    }
+  }
+}
+
+class NotificationType {
+  static const String orderDetails = 'order.details';
+}
