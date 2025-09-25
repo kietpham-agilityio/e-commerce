4e35e3f8b7f77396bc7af81313ecb8bb64a8db08
Kiet Pham <kiet.pham@asnet.com.vn>
2025-09-25 10:08:37 +0700

    update the list of authentication routes

--- e_commerce_app/lib/core/routes/router_guard.dart
+++ e_commerce_app/lib/core/routes/router_guard.dart
@@ -3,7 +3,10 @@ import 'package:go_router/go_router.dart';
 
 class RouterGuard {
   static Future<String?> authGuard(GoRouterState state) async {
-    final unAuthenList = [state.namedLocation(AppPaths.login.name)];
+    final unAuthenList = [
+      state.namedLocation(AppPaths.login.name),
+      state.namedLocation(AppPaths.signup.name),
+    ];
     // TODO: handle redirect
 
     if (unAuthenList.contains(state.matchedLocation)) {
